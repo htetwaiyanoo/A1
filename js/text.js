fa20ab97bc7401c3fd5e6e9471fbccbffab18b5a
Htet Wai Yan <htetwaiyan@Htets-MacBook-Pro.local>
2021-11-30 17:58:24 +0630

    assignment1

---
+++ js/text.js
@@ -0,0 +1,135 @@
+//+ - * / %
+
+var n1=5;
+var n2=9;
+var result=n1+n2
+console.log(result)
+
+var n3=45;
+var n4=54;
+var result = n3-n4
+console.log(result)
+
+var n5=34;
+var n6=43;
+var result=n5*n6;
+console.log(result)
+
+var n7=25;
+var n8=52;
+var result=n8/n7
+console.log(result)
+
+var n9=3;
+var n10=7;
+var result=n10%n9
+console.log(result)
+
+// += -= *= /= %=
+
+var num1=4;
+var num2=8;
+var result=num1+=num2
+console.log(result)
+
+var num3=5;
+var num4=10;
+var result=num4-=num3
+console.log(result)
+
+var num5=12;
+var num6=13;
+var result=num6*=num5
+console.log(result)
+
+var num7=3;
+var num8=6;
+var result=num8/=num7
+console.log(result)
+
+var num9=4;
+var num10=8;
+var result=num10%=num9
+console.log(result)
+
+//< > <= >= !=
+
+var c1=2;
+var c2=4;
+console.log(c1<c2)
+
+var c3=3;
+var c4=5;
+console.log(c3>c4)
+
+var c5=6;
+var c6=7;
+var result=c6<=c5
+console.log(result)
+
+var c7=8;
+var c8=9;
+var result= c8>=c7
+console.log(result)
+
+var c9=9;
+var c10=10;
+var result=c10!=c9
+console.log(result)
+
+// == ===
+
+var n=6
+var s="7"
+var result=s == n
+console.log(result)
+
+var f=5;
+var j="5";
+var result=f === j
+console.log(result)
+
+// >= <=
+
+var tx1=30
+var tx2=40
+
+if(tx2>=tx1)
+{
+console.log("The answer is true")
+ 
+}else{
+    console.log("The answer is false")
+}
+if(tx1<=tx2)
+{
+console.log("The answer is correct")
+}else{
+    console.log("The answer is wrong")
+}
+// ==
+
+var tx1=30
+var tx2=40
+
+if(tx1 == tx2)
+{
+    console.log("The answer is true")
+ 
+}else{
+    console.log("The answer is not ture")
+}
+
+// === 
+
+var tx3=25
+var tx4=23
+
+if(tx4 === tx3)
+{
+    console.log("The answer is correct")
+ 
+}else{
+    console.log("The answer is not correct")
+
+}
